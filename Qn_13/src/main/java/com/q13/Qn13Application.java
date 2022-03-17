805086ea7b408a94539336e76740754fa432ed99
yas612 <92871388+yas612@users.noreply.github.com>
2022-03-17 21:26:51 +0530

    Add files via upload

---
+++ Qn_13/src/main/java/com/q13/Qn13Application.java
@@ -0,0 +1,13 @@
+package com.q13;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Qn13Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Qn13Application.class, args);
+	}
+
+}
